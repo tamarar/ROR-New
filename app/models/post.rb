061c96a18054e72fb85618a169c54f30b1ce5163
Tamara <tamarar@gmail.com>
2014-05-05 22:30:50 -0700

    sendgrid_username and sendgrid_password

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments
+  belongs_to :user
+  
   attr_accessible :body, :title
   has_many :comments
 end
---
+++ db/migrate/20140506042611_add_user_to_posts.rb
@@ -0,0 +1,9 @@
+# db/migrate/[timestamp]_add_user_to_posts.rb
+
+class AddUserToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :user_id, :integer
+    add_index :posts, :user_id
+    
+  end
+end
