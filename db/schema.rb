3e650d4d606d57f185ab81af6e3eeb6792f89269
Tamara <tamarar@gmail.com>
2014-05-05 23:00:44 -0700

    updated after debugging 5/3/14

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140422041825) do
+ActiveRecord::Schema.define(:version => 20140506042611) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(:version => 20140422041825) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
 end
