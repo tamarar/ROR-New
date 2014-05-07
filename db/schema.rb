f20a486d2333527de985658ac644baa17cad78b5
Tamara <tamarar@gmail.com>
2014-05-06 20:52:15 -0700

    Signin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140505055529) do
+ActiveRecord::Schema.define(:version => 20140506042611) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
