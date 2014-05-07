e51403794941ef2bb302e20a52d4dcc60f67c7bc
Tamara <tamarar@gmail.com>
2014-05-06 23:20:08 -0700

    modified post.rb after error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :user
   has_many :comments
 
   default_scope { order('created_at DESC') }
