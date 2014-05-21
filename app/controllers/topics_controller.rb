a1ce205b6a344571fc4d4ab7f79d50bc7babc84d
Tamara <tamarar@gmail.com>
2014-05-20 21:37:28 -0700

    Cleaned up registration views and app layout

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,7 +20,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params[:topic])
+    @topic = Topic.new(topic_params)
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -33,11 +33,16 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params[:topic])
+    if @topic.update_attributes(topic_params)
       redirect_to @topic
     else
       flash[:error] = "Error saving topic. Please try again"
       render :edit
+    end
+    private
+
+    def topic_params
+      params.require(:topic).permit(:name, :description, :public)
     end
   end
 end
