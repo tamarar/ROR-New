885a219f42416b2220579e8dd510a40ada5e0637
Tamara <tamarar@gmail.com>
2014-05-20 20:39:03 -0700

    Fix asset precompile

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,26 +1,26 @@
 class TopicsController < ApplicationController
   def index
-  	@topics = Topic.all
-  	authorize @topic
+    @topics = Topic.all
+    authorize @topics.first
   end
 
   def new
-  	@topic = Topic.new
-  	authorize @topic
+    @topic = Topic.new
+    authorize @topic
   end
 
   def show
-  	@topic = Topic.find(params[:id])
-  	@posts = @topic.posts
+    @topic = Topic.find(params[:id])
+    @posts = @topic.posts
   end
 
   def edit
-  	@topic = Topic.find(params[:id])
-  	authorize @topic
+    @topic = Topic.find(params[:id])
+    authorize @topic
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(params[:topic])
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -33,7 +33,7 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(params[:topic])
       redirect_to @topic
     else
       flash[:error] = "Error saving topic. Please try again"
