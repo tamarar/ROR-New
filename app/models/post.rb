e7945d65f100c7bf78bd163a63d6a079bd262c50
Tamara <tamarar@gmail.com>
2014-05-23 12:30:56 -0700

    Added validation methods to post, refactored post views to use a form partial.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,9 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 5}, presence: true
+  validates :body, length: { minimum: 20}, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
