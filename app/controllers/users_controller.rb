5d21bba5cf2ba81f650732b6a77808c5175d53b6
Tamara <tamarar@gmail.com>
2014-06-02 13:21:39 -0700

    changes to users_controller.rb and user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 # app/controllers/users_controller.rb
 
-def user_params
-  params.require(:user).permit(:name, :avatar)
+class UsersController < ApplicationController
+  before_filter :authenticate_user!, except: :index
+
+  def update
+    if current_user.update_attributes(params[:user])
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path(current_user)
+    else
+      render "devise/registrations/edit"
+    end
+  end
 end
+#def user_params
+#  params.require(:user).permit(:name, :avatar)
+#end
