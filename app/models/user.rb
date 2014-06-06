00ab1a559561a9980a2dd7c1da9be5939e6f3b08
Tamara <tamarar@gmail.com>
2014-06-05 20:55:56 -0700

    Intalled and configured pagination

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :user_type, :email, :password, :password_confirmation, :remember_me, :role, :avatar, :avatar_cache
   # attr_accessible :title, :body
   has_many :posts
+  # has_many :comments
   mount_uploader :avatar, AvatarUploader
 
   def role?(base_role)
